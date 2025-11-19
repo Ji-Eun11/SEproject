cc8bf70a3f4c7ffdf8ec64fb6f020608038d8ebe
sehyun <o.sehyun@icloud.com>
2025-11-19 14:59:47 +0900

    Wizard commit

--- src/main/kotlin/com/example/demo/domain/Wizard/WizardQuestionRepository.kt
+++ src/main/kotlin/com/example/demo/domain/Wizard/WizardQuestionRepository.kt
@@ -3,6 +3,9 @@ package com.example.demo.domain.wizard.repository
 import com.example.demo.domain.wizard.WizardQuestion
 import org.springframework.data.jpa.repository.JpaRepository
 
+// 질문 조회용 리포지토리
 interface WizardQuestionRepository : JpaRepository<WizardQuestion, Long> {
+
+    // step 순으로 정렬하여 모든 질문을 가져옴
     fun findAllByOrderByStepAsc(): List<WizardQuestion>
 }
